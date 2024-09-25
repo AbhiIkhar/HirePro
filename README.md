db7a49b9a541fc38324c4d7e371a43ad771d6453
Abhishek <ikharabhishek177@gmail.com>
2024-09-25 14:36:29 +0530

    Frontend basic stuff done

---
+++ README.md
@@ -0,0 +1,61 @@
+# HirePro - Job Portal Website
+
+**HirePro** is a comprehensive job portal application designed to streamline the job search, recruitment, and application process for both job seekers and startups. Built using **React.js**, **Node.js**, **Express**, and **MongoDB**, HirePro serves as a centralized platform where users can access job listings, apply for positions, and manage their job-related activities, enhancing their overall career experience.
+
+## Key Features:
+
+### 1. Personalized Dashboard
+
+- Quick access to relevant job opportunities ensures that job seekers stay informed about open positions, application deadlines, and startup updates.
+
+### 2. Interactive Job Listings Management
+
+- Startups can manage job posts, edit listings, and track the status of applications.
+- Seamless communication between startups and job seekers ensures better collaboration.
+
+### 3. Save and Bookmark Job Posts
+
+- Users can **save job listings** they find interesting or relevant, creating a personalized collection of saved content for future reference.
+- This feature ensures that important opportunities are easily accessible and helps users stay organized during their job search.
+
+### 4. Real-time Job Updates
+
+- Startups can post real-time updates about job vacancies, ensuring that users are always informed about the latest openings, deadlines, and changes within the job market.
+
+### 5. User-Friendly Interface
+
+- The application features an intuitive and user-friendly interface, making it easy for users to navigate and access the information they need.
+
+## Benefits:
+
+- **Enhanced Communication**: Facilitates effective communication between startups and job seekers, improving coordination and collaboration during the hiring process.
+- **Timely Information**: Users receive timely updates on job postings, application statuses, and other important announcements.
+
+## Features:
+
+- **Startup Admin**: Can create, edit, delete, and view job posts.
+- **User Accounts**: Job seekers can create accounts, update profiles, and upload resumes.
+- **Form Validation**: Includes form validation for user registration, login, and job applications.
+- **Security**: JWT implementation for secure user authentication, with password encryption and decryption using **bcrypt**.
+
+## Results:
+
+| Title                   | Screenshot                                    |
+| ----------------------- | --------------------------------------------- |
+| **Landing Page**        | ![Landing Page](./photos/image.png)           |
+| **Registration Page**   | ![Registration Page](./photos/image-7.png)    |
+| **Login Page**          | ![Login Page](./photos/image-1.png)           |
+| **Recruiter Dashboard** | !![Recruiter DashBoard](./photos/image-2.png) |
+| **Job Listing**         | ![Job List](./photos/image-3.png)             |
+| **Job Description**     | ![Job Description](./photos/image-4.png)      |
+| **Browse Jobs**         | ![Browse Jobs](./photos/image-5.png)          |
+| **User Profile**        | ![User Profile](./photos/image-6.png)         |
+
+## Backend Code:
+
+You can find the backend code for the **HirePro** Job Portal at:  
+[Backend Repository](https://github.com/AbhiIkhar/HirePro-Backend)
+
+## Conclusion:
+
+**HirePro** is an efficient job portal that bridges the gap between **startups and job seekers** by providing a centralized platform for recruitment, job searching, and engagement. It improves communication, streamlines the job search process, and enhances user experience through real-time updates and personalized features.
---
+++ src/components/auth/Login.jsx
@@ -0,0 +1,158 @@
+import React, { useEffect, useState } from "react";
+import Navbar from "../shared/Navbar";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { RadioGroup } from "../ui/radio-group";
+import { Button } from "../ui/button";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { USER_API_END_POINT } from "@/utils/constant";
+import { toast } from "sonner";
+import { useDispatch, useSelector } from "react-redux";
+import { setLoading, setUser } from "@/redux/authSlice";
+import { Loader2 } from "lucide-react";
+
+const Login = () => {
+  const [input, setInput] = useState({
+    email: "",
+    password: "",
+    role: "",
+  });
+  const { loading, user } = useSelector((store) => store.auth);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const changeEventHandler = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(setLoading(true));
+      const res = await axios.post(`${USER_API_END_POINT}/login`, input, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        withCredentials: true,
+      });
+      if (res.data.success) {
+        dispatch(setUser(res.data.user));
+        navigate("/");
+        toast.success(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.response.data.message);
+    } finally {
+      dispatch(setLoading(false));
+    }
+  };
+  const handleGuestRecuiter = () => {
+    setInput({
+      email: "guestrecruiter@gmail.com",
+      password: "guest123",
+      role: "recruiter",
+    });
+  };
+  const handleGuestStudent = () => {
+    setInput({
+      email: "gueststudent@gmail.com",
+      password: "guest123",
+      role: "student",
+    });
+  };
+  useEffect(() => {
+    if (user) {
+      navigate("/");
+    }
+  }, []);
+  return (
+    <div>
+      <Navbar />
+      <div className="flex items-center justify-center max-w-7xl mx-auto">
+        <form
+          onSubmit={submitHandler}
+          className="w-1/2 border rounded-md p-4 my-10 bg-slate-50 shadow-lg"
+        >
+          <h1 className="font-bold text-[30px] mb-5 text-center text-[#68f3c2]">
+            Login
+          </h1>
+          <div className="my-2">
+            <Label>Email</Label>
+            <Input
+              type="email"
+              value={input.email}
+              name="email"
+              onChange={changeEventHandler}
+              placeholder="ikharabhishek177@gmail.com"
+            />
+          </div>
+
+          <div className="my-2">
+            <Label>Password</Label>
+            <Input
+              type="password"
+              value={input.password}
+              name="password"
+              onChange={changeEventHandler}
+              placeholder="Abhishek"
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <RadioGroup className="flex items-center gap-4 my-5">
+              <div className="flex items-center space-x-2">
+                <Input
+                  type="radio"
+                  name="role"
+                  value="student"
+                  checked={input.role === "student"}
+                  onChange={changeEventHandler}
+                  className="cursor-pointer"
+                />
+                <Label htmlFor="r1">Student</Label>
+              </div>
+              <div className="flex items-center space-x-2">
+                <Input
+                  type="radio"
+                  name="role"
+                  value="recruiter"
+                  checked={input.role === "recruiter"}
+                  onChange={changeEventHandler}
+                  className="cursor-pointer"
+                />
+                <Label htmlFor="r2">Recruiter</Label>
+              </div>
+            </RadioGroup>
+          </div>
+          {loading ? (
+            <Button className="w-full my-4">
+              {" "}
+              <Loader2 className="mr-2 h-4 w-4 animate-spin" /> Please wait{" "}
+            </Button>
+          ) : (
+            <Button type="submit" className="w-full my-4">
+              Login
+            </Button>
+          )}
+          <div className="flex flex-row justify-between content-center">
+            <span className="text-md">
+              Don't have an account?{" "}
+              <Link to="/signup" className="text-blue-600">
+                Signup
+              </Link>
+            </span>
+            <Button className="bg-red-400" onClick={handleGuestRecuiter}>
+              Guest Recruiter Login
+            </Button>
+            <Button className="bg-red-400" onClick={handleGuestStudent}>
+              Guest Student Login
+            </Button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
